bd8a82c2e0a5ae140e563851c00909fad1dbe7c5
Divyanshu Madan <iamdm99@gmail.com>
2021-03-27 12:32:19 +0530

    [clang_utils.py] documentation changes

--- bindings/python/scripts/clang_utils.py
+++ bindings/python/scripts/clang_utils.py
@@ -9,9 +9,12 @@ def getmembers_static(object, predicate=None):
     """
 
     """
-    - `getmembers` from the inspect module triggers execution, which leads to errors when types are not satisfied.
-    - So we define a custom function based on the one in the inspect module.
-    - It replaces `getattr_static` with `getattr_static` to solve the issue.
+    - `getmembers` function (from the inspect module) triggers execution instead of doing static analysis.
+    - This leads to errors, particularly on properties of classes in cindex.py, which causes segmentation errors or raises an Exception if a particular condition is not satisfied.
+    - To curb this, we need a fetch the members statically.
+    - The inspect module doesn't provide a function to do so. So we define a custom function based on the one in the inspect module.
+    
+    diff: We replace the `getattr` function call with a `getattr_static` function call in the function definition.
     """
 
     if inspect.isclass(object):
@@ -103,7 +106,9 @@ class TypeUtils:
 
 # Docstring template for the classes
 class_docstring = """
-{class_name} class utilities
+- {class_name} class utilities.
+    {link}
+- {description}
 
 `check_functions_dict`:
     - Functions that begin with "is_" i.e., checking functions
@@ -116,6 +121,18 @@ class_docstring = """
     - A list of two-tuples: (property_name: str, property_value: property)
 """
 
-CursorKindUtils.__doc__ = class_docstring.format(class_name="CursorKind")
-CursorUtils.__doc__ = class_docstring.format(class_name="Cursor")
-TypeUtils.__doc__ = class_docstring.format(class_name="Type")
+CursorKindUtils.__doc__ = class_docstring.format(
+    class_name="CursorKind",
+    link="https://github.com/llvm/llvm-project/blob/1acd9a1a29ac30044ecefb6613485d5d168f66ca/clang/bindings/python/clang/cindex.py#L657",
+    description="A CursorKind describes the kind of entity that a cursor points to.",
+)
+CursorUtils.__doc__ = class_docstring.format(
+    class_name="Cursor",
+    link="https://github.com/llvm/llvm-project/blob/1acd9a1a29ac30044ecefb6613485d5d168f66ca/clang/bindings/python/clang/cindex.py#L1415",
+    description="The Cursor class represents a reference to an element within the AST. It acts as a kind of iterator.",
+)
+TypeUtils.__doc__ = class_docstring.format(
+    class_name="Type",
+    link="https://github.com/llvm/llvm-project/blob/1acd9a1a29ac30044ecefb6613485d5d168f66ca/clang/bindings/python/clang/cindex.py#L2180",
+    description="The Type class represents the type of an element in the abstract syntax tree.",
+)
