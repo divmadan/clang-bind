609cec3cece92dfac15a57cba2485759f3ce8cc4
Divyanshu Madan <iamdm99@gmail.com>
2021-05-30 21:52:11 +0530

    [clang_utils.py] clean and modify `get_members_static`

--- bindings/python/scripts/clang_utils.py
+++ bindings/python/scripts/clang_utils.py
@@ -4,57 +4,33 @@ import clang.cindex as clang
 
 def getmembers_static(object, predicate=None):
     """
-    Return all members of an object as (name, value) pairs sorted by name.
+    Return all members of an object as (name, value) pairs sorted by name via `getattr_static`.
     Optionally, only return members that satisfy a given predicate.
+
+
+    - A static version of `get_members` function at https://github.com/python/cpython/blob/14ba761078b5ae83519e34d66ab883743912c45b/Lib/inspect.py#L444-L486
+        - `getmembers` function (from the inspect module) triggers execution instead of doing static analysis.
+        - This leads to errors, particularly on properties of classes in cindex.py, which causes segmentation errors or raises an Exception if a particular condition is not satisfied.
+        - To curb this, we fetch the members statically. We define a custom function based on the one in the inspect module.
     """
 
-    """
-    - `getmembers` function (from the inspect module) triggers execution instead of doing static analysis.
-    - This leads to errors, particularly on properties of classes in cindex.py, which causes segmentation errors or raises an Exception if a particular condition is not satisfied.
-    - To curb this, we need a fetch the members statically.
-    - The inspect module doesn't provide a function to do so. So we define a custom function based on the one in the inspect module.
-    
-    diff: We replace the `getattr` function call with a `getattr_static` function call in the function definition.
-    """
-
-    if inspect.isclass(object):
-        mro = (object,) + inspect.getmro(object)
-    else:
-        mro = ()
     results = []
-    processed = set()
     names = dir(object)
     # :dd any DynamicClassAttributes to the list of names if object is a class;
     # this may result in duplicate entries if, for example, a virtual
     # attribute with the same name as a DynamicClassAttribute exists
     try:
-        for base in object.__bases__:
-            for k, v in base.__dict__.items():
-                if isinstance(v, types.DynamicClassAttribute):
-                    names.append(k)
+        base_members = filter(
+            lambda k, v: isinstance(v, types.DynamicClassAttribute),
+            object.__bases__.__dict__.items(),
+        )
+        names.extend(base_members)
     except AttributeError:
         pass
     for key in names:
-        # First try to get the value via getattr.  Some descriptors don't
-        # like calling their __get__ (see bug #1785), so fall back to
-        # looking in the __dict__.
-        try:
-            value = inspect.getattr_static(object, key)
-            # handle the duplicate key
-            if key in processed:
-                raise AttributeError
-        except AttributeError:
-            for base in mro:
-                if key in base.__dict__:
-                    value = base.__dict__[key]
-                    break
-            else:
-                # could be a (currently) missing slot member, or a buggy
-                # __dir__; discard and move on
-                continue
+        value = inspect.getattr_static(object, key)
         if not predicate or predicate(value):
             results.append((key, value))
-        processed.add(key)
     results.sort(key=lambda pair: pair[0])
     return results
 
